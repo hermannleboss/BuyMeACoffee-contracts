f026bd48bb0f6b3c1152e19cc111bc2216cc3c5d
Hermann EHO <hermannleboss@gmail.com>
2022-10-26 16:47:48 +0000

    initial commit

---
+++ scripts/buy-coffee.js
@@ -0,0 +1,48 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat")
+
+// Returns the Ethere balance of a given address.
+async function getBalance(address) {
+  const balanceBigInt = await hre.waffle.provider.getBalance(address)
+  return hre.ethers.utils.formatEther(balanceBigInt)
+}
+
+// Logs the memos stored on-chain form coffee purchases.
+async function printMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp
+    const tipper = memo.name
+    const tipperAddress = memo.from
+    const message = memo.message
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`)
+  }
+}
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS
+
+  const lockedAmount = hre.ethers.utils.parseEther("1")
+
+  const Lock = await hre.ethers.getContractFactory("Lock")
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
+
+  await lock.deployed()
+
+  console.log(
+    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`,
+  )
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
